c57c5f399d82dd8bbf7e26aab88fe32e198ee84a
Анна Шахбазян <fatamorgana9205@gmail.com>
2020-02-26 11:25:31 +0200

    add router

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Logo from './images/logo.png'
+
+export default function App() {
+  return (
+    <div>123<img src={Logo} alt="logo" /></div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Logo from './images/logo.png'
+import App from './App'
 
-console.log('working')
-
-const App = () => (<div>123<img src={Logo} alt="logo" /></div>)
 const wrapper = document.getElementById("App");
 ReactDOM.render(<App />, wrapper)
